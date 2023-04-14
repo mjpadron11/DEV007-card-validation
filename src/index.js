5704d148444aac5ecf1fea7793502dadd4549dab
Mariem Padron <mjpadron11@gmail.com>
2023-04-14 16:01:32 -0500

    Working on isValid

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
-// import validator from './validator.js';
+import validator from './validator.js';
 const numberInput = document.getElementsByClassName("number");
-// const cardNumber = document.getElementById("card-number");
-const cardName = document.getElementById("card-name");
-const cardLastname = document.getElementById("card-lastname");
-// const cardexpiration = document.getElementById("card-exp");
-const cardCode = document.getElementById("card-code");
+const number = document.getElementById("card-number");
+const name = document.getElementById("card-name");
+const lastname = document.getElementById("card-lastname");
+const code = document.getElementById("card-code");
 const submitButton = document.getElementById("button");
 // console.log(cardNumber);
 
 const cardInfo = {
-  numberInputs: numberInput,
-  // number: cardNumber,
-  name: cardName,
-  lastname: cardLastname,
-  // expiration: cardexpiration,
-  code: cardCode
+  numberInputs: numberInput.value,
+  number: number.value,
+  name: name.value,
+  lastname: lastname.value,
+  code: code.value
 }
 
-// console.log(cardInfo.numberInputs);
+console.log(cardInfo);
 
 //Función para que sólo puedan ingresarse números en el input
-for (const input of cardInfo.numberInputs) {
-  // console.log(input);
 
-  input.addEventListener("input", (e) => {
-    const currentValue = e.target.value;
-    e.target.value = currentValue.replace(/[^0-9]/g, "");
-  });
-}
 
-submitButton.addEventListener("click", () => {
-  // e.preventDefault(e);
-  console.log(numberInput[0].value);
+
+// for (const input of numberInput) {
+//   // console.log(input);
+
+number.addEventListener("input", (e) => {
+  const currentValue = e.target.value;
+  e.target.value = currentValue.replace(/[^0-9]/g, "");
+});
+
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  validator.isValid(number.value);
+  console.log(validator);
 })
 
 
 
 
-// console.log(validator);
 
 
-// e.preventdefault para evitar que la página se refresque cuando se de click a confirmar
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,39 @@
+// import { cardInfo } from "./index";
+// console.log("🚀 ~ file: validator.js:2 ~ cardInfo:", cardInfo)
+
+
+
 const validator = {
-  // ...
+  isValid(numTarjeta){
+    const arrayNumber = numTarjeta.toString().split(",").reverse();
+    for (let i = 0; i < arrayNumber.length; i+=2){
+      if(arrayNumber >= 2){
+        arrayNumber.split("").reduce((a,b) => a + b, 0)
+      }
+    }
+  }
 };
+
+
+// Pasar el string introducido a través del input a array
+
+
+
+// aplicamos condiciones
+// const cardNumberArray = cardInfo.number.split("");
+// console.log(cardNumberArray);
+//Como introducir los numeros a un array a traves de un input
+//Revertir el array
+//Multiplicar por 2 cada indice par, se debe agregar un primer indice para que este no comience en 0
+//Sumar los numeros doblados si estos son de dos cifras
+//Sumar todo y confirmar si el resultado es un multiplo de 10
+
+//Es una función que debe devolver un booleano, la funcion recibe un parametro en este caso el input,
+// isValid: () => {
+//   const validCard = Array.from(cardNumber);
+// },
+
+
+
 
 export default validator;
