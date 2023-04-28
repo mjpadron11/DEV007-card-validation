b266d23d9672798ae1e27b0f35a8f4cc8bb25e3a
Mariem Padron <mjpadron11@gmail.com>
2023-04-27 19:15:57 -0500

    Updates

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import validator from './validator.js';
 const number = document.getElementById("card-number");
+const cardName = document.getElementById("card-name");
+const cardLastName = document.getElementById("card-lastname");
+const cardExp = document.getElementById("card-exp");
+const cardCode = document.getElementById("card-code");
 const submitButton = document.getElementById("button");
 const modalWindow = document.getElementById("modal-body");
 const acceptButton = document.getElementById("modal-button");
@@ -16,16 +20,20 @@ number.addEventListener("input", (e) => {
   e.target.value = currentValue.replace(/[^0-9]/g, "");
 });
 
-
 submitButton.addEventListener("click", (e) => {
   const hashedNumber = validator.maskify(number.value);
-
+  const isAnyValueEmpty = number.value && cardName.value && cardLastName.value && cardExp.value && cardCode.value
   e.preventDefault();
-  if(validator.isValid(number.value)){
-    maskifiedCardNumber.innerText = hashedNumber;
-    showModal();
+  if (!isAnyValueEmpty) {
+    alert("Por favor, completa todos los campos.");
+    return;
   }else{
-    alert("El número ingresado en incorrecto. Por favor, ingresa un número de tarjeta válido.")
+    if(validator.isValid(number.value)){
+      maskifiedCardNumber.innerText = hashedNumber;
+      showModal();
+    }else{
+      alert("El número ingresado en incorrecto. Por favor, ingresa un número de tarjeta válido.")
+    }
   }
 })
 
